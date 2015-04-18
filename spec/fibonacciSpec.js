cc7699b1ce4ad8bfb7bbf636fe56427815920374
Sara OC <saramoo@hotmail.com>
2015-04-18 21:11:18 +0100

    adds one as second number in list

--- spec/fibonacciSpec.js
+++ spec/fibonacciSpec.js
@@ -3,4 +3,10 @@ describe('Fibonacci', function () {
         fibonacci = new Fibonacci;
         expect(fibonacci.list[0]).toBe(0);
     });
+
+    it('can add one as second number in the list', function () {
+        fibonacci = new Fibonacci;
+        fibonacci.add_number(1);
+        expect(fibonacci.list).toEqual([0, 1]);
+    });
 });
--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,3 +1,7 @@
 var Fibonacci = function(){
     this.list = [0];
 };
+
+Fibonacci.prototype.add_number = function(number) {
+    this.list.push(number)
+};
