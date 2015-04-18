1894b5ee03bbe4c0b10a0a01b4406571c6f8403a
Sara OC <saramoo@hotmail.com>
2015-04-18 22:17:12 +0100

    know the final two numbers in the array

--- spec/fibonacciSpec.js
+++ spec/fibonacciSpec.js
@@ -1,12 +1,25 @@
+"use strict";
+
+var describe, Fibonacci, fibonacci, beforeEach, it, expect;
+
 describe('Fibonacci', function () {
+    beforeEach(function () {
+        fibonacci = new Fibonacci();
+    });
+
     it('has zero as first number in the list', function () {
-        fibonacci = new Fibonacci;
         expect(fibonacci.list[0]).toBe(0);
     });
 
-    it('can add one as second number in the list', function () {
-        fibonacci = new Fibonacci;
-        fibonacci.add_number(1);
+    it('can add number to the list', function () {
+        fibonacci.include_number_in_array(1);
         expect(fibonacci.list).toEqual([0, 1]);
+    });
+
+    it('knows the final two numbers in the array', function () {
+        fibonacci.include_number_in_array(1);
+        fibonacci.include_number_in_array(2);
+        fibonacci.include_number_in_array(3);
+        expect(fibonacci.last_two_numbers()).toEqual([2, 3]);
     });
 });
--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,7 +1,16 @@
-var Fibonacci = function(){
+var Fibonacci = function () {
+    "use strict";
     this.list = [0];
 };
 
-Fibonacci.prototype.add_number = function(number) {
-    this.list.push(number)
+Fibonacci.prototype.include_number_in_array = function (number) {
+    "use strict";
+    this.list.push(number);
+};
+
+Fibonacci.prototype.last_two_numbers = function () {
+    "use strict";
+    var last_number = this.list[this.list.length-1];
+    var second_to_last_number = this.list[this.list.length-2];
+    return [second_to_last_number, last_number];
 };
